0f6e7bce260ba206fcf2cf45e72c9e789d386292
Danilo Alberto <danialberto42@gmail.com>
2024-08-01 15:31:16 -0300

    adicionando a função salvar

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -23,9 +24,36 @@ public class ProdutosDAO {
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
     public void cadastrarProduto (ProdutosDTO produto){
+        String sql = "INSERT INTO produtos (nome, valor,status) VALUES (?, ?, ?)";
+        try{
+        //criando o metodo de salvar 
+        conn = new conectaDAO().connectDB();
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, produto.getNome());
+        prep.setInt(2, produto.getValor());
+        prep.setString(3, produto.getStatus());
         
+        // execultando a sql
+        int linhasafetadas  =prep.executeUpdate();
         
-        //conn = new conectaDAO().connectDB();
+        // se efetuado com sucesso 
+        if(linhasafetadas >0){
+            System.out.println("produto cadastrado com sucesso");
+        }else{
+            System.out.println("falha no cadastro do produto");
+        }
+         
+        }catch(SQLException e){
+            System.out.println("erro ao cadastrar o produto:"+e.getMessage());
+        }finally{
+            //fechando as coneccoes
+            try{
+            if(prep != null)prep.close();
+            if(conn != null) conn.close();  
+        }catch(SQLException e){
+                System.out.println("Erro ao fechar os recursos "+ e.getMessage());
+            
+        }}
         
         
     }
